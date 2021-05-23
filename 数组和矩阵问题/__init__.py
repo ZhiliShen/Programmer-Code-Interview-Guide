a3e48fa13a9d57307b6fa1b693c8cf0cc9d116c2
ZhiliShen <zhilishen@163.com>
2021-05-23 20:22:56 +0800

    未排序数组中累加和为给定值的最长子数组系列问题

---
+++ 数组和矩阵问题/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*- # 
+# @Time    : 2021-05-23 19:53
+# @Email   : zhilishen@smail.nju.edu.cn
+# @Author  : Zhili Shen
+# @File    : __init__.py
+# @Notice  :
---
+++ 数组和矩阵问题/未排序数组中累加和为给定值的最长子数组系列问题/test1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*- # 
+# @Time    : 2021-05-23 19:53
+# @Email   : zhilishen@smail.nju.edu.cn
+# @Author  : Zhili Shen
+# @File    : test1.py
+# @Notice  :
+import sys
+from typing import List
+
+
+def max_length(arr: List, k: int):
+    if arr is None or len(arr) == 0:
+        return 0
+    dic = {0: -1}
+    temp_sum = 0
+    max_len = 0
+    for i, cur in enumerate(arr):
+        temp_sum += cur
+        dic.setdefault(temp_sum, i)
+        j = dic.get(temp_sum-k)
+        if j is not None:
+            max_len = max(i-j, max_len)
+    return max_len
+
+
+if __name__ == "__main__":
+    for idx, line in enumerate(sys.stdin):
+        if idx == 0:
+            temp = [int(k) for k in line.split()]
+            length = temp[0]
+            k = temp[-1]
+        elif idx == 1:
+            temp = [int(k) for k in line.split()]
+            break
+
+    print(max_length(temp, k))
