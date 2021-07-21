47850e5a6df00afa5f63ac5525089ae872c3b9d7
zhilishen <zhilishen@163.com>
2021-07-21 19:59:13 +0800

    找到二叉树中的最大搜索二叉子树

--- 二叉树问题/找到二叉树中的最大搜索二叉子树/test.py
+++ 二叉树问题/找到二叉树中的最大搜索二叉子树/test.py
@@ -22,7 +22,7 @@ def process(x: Node) -> return_type:
     l_data = process(x.left)
     r_data = process(x.right)
     min_value = min(x.value, l_data.min_value, r_data.min_value)  # x.value!
-    max_value = max(x.value,l_data.max_value, r_data.max_value)  # x.value!
+    max_value = max(x.value, l_data.max_value, r_data.max_value)  # x.value!
     max_bst_size = max(l_data.max_bst_size, r_data.max_bst_size)
     max_bst_head = l_data.max_bst_head if l_data.max_bst_size >= r_data.max_bst_size else r_data.max_bst_head
     if l_data.max_bst_head == x.left and r_data.max_bst_head == x.right and l_data.max_value < x.value < r_data.min_value:
