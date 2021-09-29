a4012f8cae555ab92e9bc074a625b8b42f280fbe
ZhiliShen <zhilishen@163.com>
2021-09-29 18:02:59 +0800

    判断t1树中是否有与t2树拓扑结构完全相同的子树

--- 二叉树问题/判断t1树中是否有与t2树拓扑结构完全相同的子树/test.py
+++ 二叉树问题/判断t1树中是否有与t2树拓扑结构完全相同的子树/test.py
@@ -4,12 +4,9 @@
 # @Author  : Zhili Shen
 # @File    : test.py
 # @Notice  :
-from typing import List
-from collections import deque
-from 二叉树问题.如何较为直观地打印二叉树.test import print_tree
-from 二叉树问题.utils.utils import Node, build_2_tree_from_stdin, build_tree_from_stdin, print_list, build_2_tree_from_txt
+from 二叉树问题.utils.utils import Node, build_2_tree_from_txt
 from 二叉树问题.二叉树的序列化和反序列化.test import serial_by_pre
-from 其他问题.KMP算法.test import search
+from 其他题目.KMP算法.test import search
 
 
 def contains(t1: Node, t2: Node):
