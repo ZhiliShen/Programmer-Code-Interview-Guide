10c370b8183697feff29228a9e1e10a3caff63da
ZhiliShen <zhilishen@163.com>
2021-10-08 19:44:25 +0800

    判断两个字符串是否互为变形词

---
+++ 字符串问题/判断两个字符串是否互为变形词/test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*- # 
+# @Time    : 2021-10-08 19:15
+# @Email   : zhilishen@smail.nju.edu.cn
+# @Author  : Zhili Shen
+# @File    : test.py
+# @Notice  :
+
+def is_deformation(a_str, b_str):
+    if len(a_str) != len(b_str):
+        return 'false'
+    char_map = [0 for i in range(256)]
+    for temp in a_str:
+        char_map[ord(temp)] += 1
+    for temp in b_str:
+        char_map[ord(temp)] -= 1
+        if char_map[ord(temp)] < 0:
+            return 'false'
+
+    return 'true'
+
+
+if __name__ == "__main__":
+    a, b = map(int, input().split())
+    str1 = input()
+    str2 = input()
+    print(is_deformation(str1, str2))
